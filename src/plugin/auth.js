5cd0e7fc01125d12c72b3962607d331f95525949
Paola <pao_jb_mc_@hotmail.com>
2019-02-18 12:02:19 -0600

    add custom plugin for auth

---
+++ src/plugin/auth.js
@@ -0,0 +1,21 @@
+const fastify = require('fastify')({ logger: true })
+    , boom    = require('boom')
+    , fp = require('fastify-plugin')
+
+async function customJwtAuth(fastify, opts) {
+  fastify
+  .decorate("authenticate", async function(req, reply, done) {
+    try {
+      console.log('try');
+      req.user = await fastify.jwt.verify(req.headers.authorization)
+      console.log(req.user)
+      done()
+    } catch (err) {
+      console.log('catch');
+      reply.send('Usuario sin autorización')
+      done()
+    }
+  })
+}
+
+module.exports = fp(customJwtAuth)
